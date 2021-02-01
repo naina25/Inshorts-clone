19bf03fdf94b388a1373b42e0fccc21bbc6a8b0a
naina <nanu02d8@gmail.com>
2021-02-01 19:02:09 +0530

    add news description and source, plus render date and time also

--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import "./NewsCard.css";
 
 const NewsCard = ({ newsItem }) => {
+  const fullDate = new Date(newsItem.publishedAt);
+  const date = fullDate.toString().split(" ");
+  const hour = parseInt(date[4].substring(0, 2));
+  const time = hour > 12;
+
+  const renderSwitch = (day) => {
+    switch (day) {
+      case "Mon":
+        return "Monday";
+        break;
+      case "Tue":
+        return "Tuesday";
+        break;
+      case "Wed":
+        return "Wednesday";
+        break;
+      case "Thu":
+        return "Thursday";
+        break;
+      case "Fri":
+        return "Friday";
+        break;
+      case "Sat":
+        return "Saturday";
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <div>
+    <div className="newsCard">
       <img
         src={
           newsItem.urlToImage
@@ -12,9 +44,10 @@ const NewsCard = ({ newsItem }) => {
         alt={newsItem.title}
         className="newsImage"
       />
-      <div className="newsText">
+      <div className="upperText">
         <div>
           <span className="title">{newsItem.title}</span>
+          <br />
           <p className="author">
             <a
               href={newsItem.url}
@@ -23,9 +56,31 @@ const NewsCard = ({ newsItem }) => {
               <b>short </b>
             </a>
             <span className="mutedText">
-              by {newsItem.author ? newsItem.author : "anonymous"} / {}{" "}
+              by {newsItem.author ? newsItem.author : "anonymous"} /{" "}
+              {time
+                ? hour - 12 < 10
+                  ? `0${hour - 12}:${date[4].substring(3, 5)} pm`
+                  : `${hour - 12}:${date[4].substring(3, 5)} pm`
+                : `${hour} : ${date[4].substring(3, 5)} am`}{" "}
+              on {date[2]} {date[1]} {date[3]}, {renderSwitch(date[0])}
             </span>
           </p>
+        </div>
+        <div className="lowerText">
+          <div className="description">
+            {newsItem.description
+              ? newsItem.description
+              : "*** This article has no description ***"}
+          </div>
+          <span className="readmore">
+            read more at
+            <a
+              href={newsItem.url}
+              target="__blank"
+              style={{ color: "black", textDecoration: "none" }}>
+              <b> {newsItem.source.name}</b>
+            </a>
+          </span>
         </div>
       </div>
     </div>
